0d699209f7d7c91b649251e835a87abd3a0b278a
kirito-udit <uditkotecha60@gmail.com>
2020-12-22 14:06:18 +0530

    ...

---
+++ main.py
@@ -0,0 +1,67 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        engine.say("Konichiwa darling, orewa Zero Two dais, nani soreh?")
+        print("Hi Darling, I am Zero Two, your assistant, how can I serve you?")
+        engine.runAndWait()
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'zero two' in command:
+                command = command.replace('zero two', '')
+                print(command)
+            talk('chotto matte')
+    except:
+        pass
+    return command
+
+
+def run_bot():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'stop' in command:
+        talk('Miss you, get back ASAP')
+        exit()
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_bot()
